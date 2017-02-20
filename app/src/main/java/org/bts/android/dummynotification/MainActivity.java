8a39231940a341926ec6814b67b01fe0408755e7
pedrodelmonte <pdelmonte@gmail.com>
2017-02-20 10:18:28 +0100

    added button functionality

--- app/src/main/java/org/bts/android/dummynotification/MainActivity.java
+++ app/src/main/java/org/bts/android/dummynotification/MainActivity.java
@@ -2,12 +2,41 @@ package org.bts.android.dummynotification;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnStandardNotification = (Button) findViewById(R.id.button_to_notify);
+        btnStandardNotification.setOnClickListener(this);
+        final Button btnCustomNotification = (Button) findViewById(R.id.button_to_custom_notify);
+        btnCustomNotification.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View whichView) {
+
+        switch(whichView.getId()) {
+            case R.id.button_to_notify:
+                Log.i(MainActivity.TAG, "Add Notification Button clicked");
+
+                break;
+
+            case R.id.button_to_custom_notify:
+                Log.i(MainActivity.TAG, "Custom Add Notification Button clicked");
+
+                break;
+            default:
+                Log.i(MainActivity.TAG,"Clicked Item not registered");
+        }
+
     }
 }
